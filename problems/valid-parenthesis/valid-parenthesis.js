d85e93a4ebc0b12c23f20f5edb0b9086e12cb5e9
Spiryaev <kaline-son@ya.ru>
2023-08-22 00:06:01 +0700

    add function for valid parenthesis

--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,24 @@
 // https://leetcode.com/problems/valid-parentheses/
-
+const charSet = {
+  "(": ")",
+  "{": "}",
+  "[": "]",
+};
 export function isValid(str) {
-
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char in charSet) {
+      stack.push(char);
+      continue;
+    }
+    const lastOpenChar = stack.pop();
+    if (char !== charSet[lastOpenChar]) {
+      return false;
+    }
+  }
+  return true;
 }
