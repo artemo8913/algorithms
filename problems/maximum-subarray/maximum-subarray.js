ed3a4bdfbe5788bd2779f3a5387784b3cc7e0c59
Spiryaev <kaline-son@ya.ru>
2023-08-22 01:50:49 +0700

    create func find maximum subarray

--- problems/maximum-subarray/maximum-subarray.js
+++ problems/maximum-subarray/maximum-subarray.js
@@ -1,5 +1,11 @@
 // https://leetcode.com/problems/maximum-subarray/
 
 export function maximumSubarray(arr) {
-
+  const prefSum = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    const num = arr[i];
+    const numAndPref = num + prefSum[prefSum.length - 1];
+    prefSum.push(Math.max(num, numAndPref));
+  }
+  return Math.max(...prefSum);
 }
