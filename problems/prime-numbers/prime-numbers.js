58434fe70007f610164398acf9afa02661335f16
Spiryaev <kaline-son@ya.ru>
2023-08-22 00:25:24 +0700

    add function for creating prime numbers list

--- problems/prime-numbers/prime-numbers.js
+++ problems/prime-numbers/prime-numbers.js
@@ -1,7 +1,29 @@
 // https://ru.wikipedia.org/wiki/Решето_Эратосфена
 
 export function primeNumbers(num) {
-    const result = [];
-
-    return result;
+  const a = 1;
+  const result = [];
+  for (let i = 2; i <= num; i++) {
+    if (isNumberPrime(i)) {
+      result.push(i);
+    }
+  }
+  return result;
 }
+
+function isNumberPrime(num) {
+  const sqrtOfNum = Math.sqrt(num);
+  if (num === 2) {
+    return true;
+  }
+  for (let i = 2; i <= Math.ceil(sqrtOfNum); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(primeNumbers(2));
+console.log(primeNumbers(4));
+console.log(primeNumbers(20));
+console.log(primeNumbers(156));
