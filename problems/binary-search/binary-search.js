1a825808b1c1a898e4263fc447c6545d3d1c6087
Spiryaev <kaline-son@ya.ru>
2023-08-22 01:10:33 +0700

    don't resolve :((

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,20 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 export function binarySearch(arr, value) {
-
+  if (arr.length === 0) {
+    return undefined;
+  }
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+  while (rightIndex - leftIndex > 1) {
+    const midIndex = Math.floor((rightIndex + leftIndex) / 2);
+    const midVal = arr[midIndex];
+    if (midVal >= value) {
+      rightIndex = midIndex;
+    } else {
+      leftIndex = midIndex;
+    }
+  }
+  console.log(`rightIndex: ${rightIndex}, val: ${arr[rightIndex]}`);
+  return rightIndex;
 }
