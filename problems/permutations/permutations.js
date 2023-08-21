233ca79a3328e1c0f488992f99afc46994bcb65b
Spiryaev <kaline-son@ya.ru>
2023-08-22 01:45:16 +0700

    create permutations

--- problems/permutations/permutations.js
+++ problems/permutations/permutations.js
@@ -1,7 +1,27 @@
 // https://leetcode.com/problems/permutations/
 
-export function permutations(n) {
-    const result = [];
+function permutations(n) {
+  const array = [];
+  for (let i = 1; i <= n; i++) {
+    array.push(i);
+  }
 
-    return result;
+  return createPermutations(array);
+}
+function createPermutations(inputArr) {
+  const result = [];
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m);
+      return;
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        const curr = arr.slice();
+        const next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+  permute(inputArr);
+  return result;
 }
